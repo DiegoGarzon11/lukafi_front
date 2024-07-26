951499d20e8b7b13fab3b8e7abe8ee6a313d32cf
sebastian <sebasga12345@gmail.com>
2024-07-25 20:03:38 -0500

    404

--- src/pages/Page404.tsx
+++ src/pages/Page404.tsx
@@ -2,12 +2,17 @@ import {Link} from 'react-router-dom';
 
 export const PageNotFound = () => {
 	return (
-		<main className='flex justify-center '>
-			<div className='fixed bg-slate-400 mt-40 w-2/4 h-2/3 rounded-lg flex flex-col justify-center items-center border '>
-				<h1 className='text-9xl'>404</h1>
-				<p className='text-2xl'>Esta pagina no se encuentra</p>
-				<Link to='/'>
-					<button className='mt-20 border bg-slate-200 border-slate-900 p-3 rounded-xl hover:scale-105'>Volver al inicio</button>
+		<main className='flex flex-col items-center justify-center bg-slate-200 dark:bg-slate-950 h-screen '>
+			<div className='grid grid-cols-2 justify-items-center gap-y-8 items-center w-2/5 my-10'>
+				<h1 className='text-9xl text-slate-800 dark:text-white font-light'>404</h1>
+				<img src='images/img_pageNF.png' width={280} alt='' className='pl-10 row-span-2 pt-10' />
+				<p className='text-xl font-mono text-center text-white  text-pretty' style={{textShadow: `1px 1px 3px green`}}>
+					Oops! parece que te fuiste por el camino equivocada hacia una mejor financiación
+				</p>
+				<Link to='/auth'>
+					<button className='bg-slate-950 text-white border border-slate-900 dark:border-white p-3 rounded-xl hover:scale-110 ease-in duration-150'>
+						Volver al inicio
+					</button>
 				</Link>
 			</div>
 		</main>
