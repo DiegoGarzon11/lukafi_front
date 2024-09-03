c7534a541f109437984585f174311900b6e18bb2
Diego Garzon <diegogarzon357@gmail.com>
2024-09-02 19:10:54 -0500

    bug

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,19 +1,34 @@
-import {Routes, Route, Navigate} from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
-import {Home} from './pages/Home';
-import {Dashboard} from './pages/Dashboard';
-import {PageNotFound} from './pages/Page404';
+import { Home } from './pages/Home';
+import { Dashboard } from './pages/Dashboard';
+import { PageNotFound } from './pages/Page404';
 import Auth from './auth/Auth';
-import {WalletComponent} from './pages/Wallet';
+import { WalletComponent } from './pages/Wallet';
 
 const isAuthenticated = localStorage.token;
 const RoutesManager = () => (
 	<Routes>
-		<Route path='/' element={isAuthenticated ? <Navigate to='/dashboard' /> : <Home />} />
-		<Route path='auth' element={isAuthenticated ? <Navigate to='/dashboard' /> : <Auth />} />
-		<Route path='dashboard' element={isAuthenticated ? <Dashboard /> : <Navigate to='/auth' />} />
-		<Route path='/wallet' element={isAuthenticated ? <WalletComponent /> : <Navigate to='/auth' />} />
-		<Route path='*' element={<PageNotFound />} />
+		<Route
+			path='/'
+			element={isAuthenticated ? <Navigate to='/dashboard' /> : <Home />}
+		/>
+		<Route
+			path='auth'
+			element={isAuthenticated ? <Navigate to='/dashboard' /> : <Auth />}
+		/>
+		<Route
+			path='dashboard'
+			element={isAuthenticated ? <Dashboard /> : <Navigate to='/auth' />}
+		/>
+		<Route
+			path='/wallet'
+			element={isAuthenticated ? <WalletComponent /> : <Navigate to='/auth' />}
+		/>
+		<Route
+			path='*'
+			element={<PageNotFound />}
+		/>
 	</Routes>
 );
 
