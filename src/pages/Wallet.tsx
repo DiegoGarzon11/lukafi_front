ecc4e591ebe2f3fbb32ba46831b61504afd30e19
Diego Garzon <diegogarzon357@gmail.com>
2024-10-22 16:05:46 -0500

    responsive dashboard, deudas

--- src/pages/Wallet.tsx
+++ src/pages/Wallet.tsx
@@ -1,15 +1,33 @@
-import { ChartFinance } from "@/components/core/Charts";
+import { ChartFinance } from '@/components/core/Charts';
 
 export const WalletComponent = () => {
 	return (
-		<section className='flex flex-col md:flex-row  items-center justify-between h-full pt-20 p-5 gap-5 dark:bg-dark_primary_color bg-white font-thin'>
-			
-			<div className='flex flex-col justify-between h-full w-full md:w-2/3 rounded-3xl p-8 shadow-sm shadow-zinc-900/90'>
-				<ChartFinance/>
-			</div>
-			<div>
-				selecciona tu tarjeta
-			</div>
-		</section>
+		<main className='  h-screen pt-20 p-3 ark:bg-dark_primary_color  font-thin bg-dark_primary_color grid grid-cols-2 '>
+			<section>
+				ahfgsdkjf
+			</section>
+			<section className='flex flex-col gap-5'>
+				<div className='flex flex-col justify-between w-full rounded-3xl p-8 shadow-sm shadow-zinc-900/90'>
+					<ChartFinance />
+				</div>
+				<div className='bg-black flex justify-center p-3'>
+					selecciona tu tarjeta
+					<article className=' text-white flex flex-col justify-between  bg-gradient-to-r  to-green-400  from-gray-700 w-full md:w-2/5 rounded-3xl p-8 shadow-xl shadow-zinc-900/90'>
+						<div className='flex justify-between'>
+							<p className='text-xl font-bold'>diego</p>
+							<p className='text-5xl font-bold tracking-widest  '>LUFI</p>
+						</div>
+						<div className='flex flex-col'>
+							<p className=' font-semibold'>12312873</p>
+							<p className=' font-semibold text-3xl tracking-wider'>10.000.000</p>
+						</div>
+						<div className='flex justify-between '>
+							<p className='font-semibold'>123123-123-12312-122</p>
+							<p>20/10/24</p>
+						</div>
+					</article>
+				</div>
+			</section>
+		</main>
 	);
 };
