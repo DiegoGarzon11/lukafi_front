eb1fb2451ae356c654182151de2afef1d79dda20
sebastian <sebasga12345@gmail.com>
2025-03-26 20:14:50 -0500

    ticket 100

--- src/components/others/Card.tsx
+++ src/components/others/Card.tsx
@@ -1,9 +1,9 @@
-import { Info } from '@/assets/icons/Svg';
-import { useTranslation } from 'react-i18next';
-import { Link } from 'react-router-dom';
+import {Info} from '@/assets/icons/Svg';
+import {useTranslation} from 'react-i18next';
+import {Link} from 'react-router-dom';
 
-export const Card = ({ title, info, img }) => {
-	const { t, i18n } = useTranslation();
+export const Card = ({title, info, img}) => {
+	const {t, i18n} = useTranslation();
 	i18n.changeLanguage();
 	return (
 		<div className='border border-black border-b-4  rounded-4xl p-4 dark:bg-dark_primary_color w-full'>
@@ -21,11 +21,7 @@ export const Card = ({ title, info, img }) => {
 					</div>
 				</div>
 				<div className=''>
-					<img
-						className='w-36'
-						src={img}
-						alt=''
-					/>
+					<img className='w-36' src={img} alt='' />
 				</div>
 			</div>
 		</div>
